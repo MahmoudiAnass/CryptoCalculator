1c58de4d1a23ce850ec416847309fed9eba07b38
anass <94812387+2ouchen@users.noreply.github.com>
2024-09-09 00:03:45 +0100

    Your commit message

---
+++ cryptomasters/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Crypto Masters
+</h1>
+    
+<form>
+
+<label >Amount</label>
+<input type="text">
+<label >Crypto</label>
+<select>
+    <option >
+        BTC
+    </option>
+</select>
+</form>
+</body>
+</html>
